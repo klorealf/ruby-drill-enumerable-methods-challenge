2f0f22e31f39161f258ddb73ba84c34a838fa990
Thomas Rafferty & bradsloan <pair+ThomasRafferty.bsloan3@devbootcamp.com>
2017-08-29 11:18:06 -0400

    Complete release 2

--- filter.rb
+++ filter.rb
@@ -1,11 +1,11 @@
 def odd_integers(numbers)
-  numbers.select {|number| number.odd?}
+  odds = numbers.select {|number| number.odd?}
 end
 
 def long_strings(strings, minimum_length)
-  strings.select {|string| string.length >= minimum_length}
+  longs = strings.select {|string| string.length >= minimum_length}
 end
 
 def multiples_of(possible_multiples, number)
-  possible_multiples.select {|possible_multiple| possible_multiple % number == 0}
+  multiples =  possible_multiples.select {|possible_multiple| possible_multiple % number == 0}
 end
