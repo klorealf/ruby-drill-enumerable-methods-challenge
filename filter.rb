b5aac3f25e81beea72f01ea492b96066c169c3ee
Chris Bordes & David Skaggs <pair+cbordes8.davidtskaggs@devbootcamp.com>
2017-08-29 11:22:22 -0400

    Adjust spacing

--- filter.rb
+++ filter.rb
@@ -1,13 +1,13 @@
 def odd_integers(numbers)
-numbers.select { |number| number.odd? }
+  numbers.select { |number| number.odd? }
 end
 
 
 
 def long_strings(strings, minimum_length)
-strings.select { |string| string.length >= minimum_length }
+  strings.select { |string| string.length >= minimum_length }
 end
 
 def multiples_of(possible_multiples, number)
-possible_multiples.select { |possible_multiple| possible_multiple % number == 0 }
+  possible_multiples.select { |possible_multiple| possible_multiple % number == 0 }
 end
