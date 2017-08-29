40013804af9373307df0e9d39caacedf6a1c0c51
Amber Maillard <amber.maillard23@gmail.com>
2017-08-29 11:12:28 -0400

    Update .each to .map for Release 2

--- transform.rb
+++ transform.rb
@@ -1,29 +1,17 @@
 def exclamations(strings)
-  loud_strings = []
-
-  strings.each do |string|
-    loud_strings << string.upcase + "!"
+  strings.map do |string|
+    string.upcase + "!"
   end
-
-  loud_strings
 end
 
 def square_all(numbers)
-  squares = []
-
-  numbers.each do |number|
-    squares << number ** 2
+  numbers.map do |number|
+    number ** 2
   end
-
-  squares
 end
 
 def first_letters(words)
-  letters = []
-
-  words.each do |word|
-    letters << word[0]
+  words.map do |word|
+    word[0]
   end
-
-  letters
 end
