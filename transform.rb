5a9b95e756cfc9daaa12ac043c63aa72ff4ccef1
klorealf & Samory Harris <pair+klorealf.samory070@devbootcamp.com>
2017-08-29 12:07:46 -0400

    refactor exclamations, square_all, first_letter methods

--- transform.rb
+++ transform.rb
@@ -1,29 +1,40 @@
 def exclamations(strings)
-  loud_strings = []
+  # loud_strings = []
 
-  strings.each do |string|
-    loud_strings << string.upcase + "!"
+  # strings.each do |string|
+  #   loud_strings << string.upcase + "!"
+  # end
+
+  # loud_strings
+  strings.map do |string|
+    string.upcase + "!"
   end
-
-  loud_strings
 end
 
 def square_all(numbers)
-  squares = []
+  # squares = []
 
-  numbers.each do |number|
-    squares << number ** 2
+  # numbers.each do |number|
+  #   squares << number ** 2
+  # end
+
+  # squares
+  numbers.map do |number|
+    number ** 2
   end
-
-  squares
 end
 
 def first_letters(words)
-  letters = []
+  # letters = []
 
-  words.each do |word|
-    letters << word[0]
+  # words.each do |word|
+  #   letters << word[0]
+  # end
+
+  # letters
+
+  words.map do |word|
+    word[0]
   end
-
-  letters
 end
+
