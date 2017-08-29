add7faaa5af55eb8253dd5ba5059417f82e84b2c
Amber Maillard <amber.maillard23@gmail.com>
2017-08-29 11:08:27 -0400

    Update .each to .select for release 1

--- filter.rb
+++ filter.rb
@@ -1,33 +1,18 @@
 def odd_integers(numbers)
-  odds = []
-
-  numbers.each do |number|
-    odds << number if number.odd?
+  numbers.select do |number|
+    number.odd?
   end
-
-  odds
 end
 
 def long_strings(strings, minimum_length)
-  longs = []
 
-  strings.each do |string|
-    if string.length >= minimum_length
-      longs << string
-    end
+  strings.select do |string|
+    string.length >= minimum_length
   end
-
-  longs
 end
 
 def multiples_of(possible_multiples, number)
-  multiples = []
-
-  possible_multiples.each do |possible_multiple|
-    if possible_multiple % number == 0
-      multiples << possible_multiple
-    end
+  possible_multiples.select do |possible_multiple|
+    possible_multiple % number == 0
   end
-
-  multiples
 end
