46cc1c8ee70b458bf0fe654dbc0e755c2c5f3173
Andres Arias & Andreas Bach Landgrebe <pair+aarias89.grebes15@devbootcamp.com>
2017-08-29 12:35:00 -0400

    Add code for Release 4 pass rspec tests

--- working_with_enumerables.rb
+++ working_with_enumerables.rb
@@ -1,62 +1,75 @@
 def containing_quotes(sentences)
-  sentences_with_quotes = []
+  #sentences_with_quotes = []
 
-  sentences.each do |sentence|
-    sentences_with_quotes << sentence if contains_quote?(sentence)
-  end
+  #sentences.each do |sentence|
+  #  sentences_with_quotes << sentence if contains_quote?(sentence)
+  #end
 
-  sentences_with_quotes
+  sentences.select {|sentence| sentence if contains_quote?(sentence)}
+
+  #sentences_with_quotes
 end
 
 def balance(expenses, starting_balance)
-  remaining_balance = starting_balance
+  # remaining_balance = starting_balance
+  expenses.reduce(starting_balance){|balance,expenses| balance - expenses}
+  # expenses.each do |expense|
+  #   remaining_balance -= expense
+  # end
 
-  expenses.each do |expense|
-    remaining_balance -= expense
-  end
-
-  remaining_balance
+  # remaining_balance
 end
 
 def even_length_word(words)
-  words.each do |word|
-    return word if word.length.even? && !word.empty?
-  end
+  words.find{|word| word.length.even? && !word.empty?}
+  #words.each do |word|
+    #return word if word.length.even? && !word.empty?
+  #end
 
-  nil
+  #nil
+  # words.select {|word| word if word.length.even?}
 end
 
 def snippets(sentences, desired_word_count = 3)
-  snipped_sentences = []
 
-  sentences.each do |sentence|
-    snipped_sentences << snippet(sentence, desired_word_count)
-  end
+  sentences.map{|sentence| snippet(sentence, desired_word_count)}
+  # snipped_sentences = []
 
-  snipped_sentences
+  # sentences.each do |sentence|
+  #   snipped_sentences << snippet(sentence, desired_word_count)
+  # end
+
+  # snipped_sentences
 end
 
 def initials(names)
-  names_as_initials = []
+  #names_as_initials = []
 
-  names.each do |name|
-    names_as_initials << convert_to_initials(name)
-  end
+  #names.each do |name|
+  #  names_as_initials << convert_to_initials(name)
+  #end
 
-  names_as_initials
+  #names_as_initials
+  names.map{|name| convert_to_initials(name)}
 end
 
 def pair_abbreviations(pair_data)
   pairs = {}
-
-  pair_data.each do |data|
-    abbreviation = data[0]
-    full_form    = data[1]
-
-    pairs[full_form] = abbreviation
-  end
-
+  pair_data.map {|full, abv| pairs[abv] = full}
   pairs
+
+
+
+  # pairs = {}
+
+  # pair_data.each do |data|
+  #   abbreviation = data[0]
+  #   full_form    = data[1]
+
+  #   pairs[full_form] = abbreviation
+  # end
+
+  # pairs
 end
 
 
