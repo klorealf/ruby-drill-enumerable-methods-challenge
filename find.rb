7d35eaacac089fc6a5d9f4d542335c87b270b2a9
klorealf & Samory Harris <pair+klorealf.samory070@devbootcamp.com>
2017-08-29 11:26:01 -0400

    refactor first_under method

--- find.rb
+++ find.rb
@@ -1,10 +1,13 @@
 def first_under(numbers, limit)
-  numbers.each do |number|
-    return number if number < limit
+  # numbers.each do |number|
+  #   return number if number < limit
+  # end
+  numbers.find do |number|
+    number < limit
   end
-
-  nil
 end
+
+p first_under([13, 21, 7, 0, 11, 106], 10)
 
 def starts_with(strings, letter)
   strings.each do |string|
