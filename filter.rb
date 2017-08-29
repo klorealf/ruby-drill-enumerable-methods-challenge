155f52510bcdd3143c8c7cecbbd25cff69e393c4
Catherine Rice & Chris Camp <pair+clr2107.cpcamp2@devbootcamp.com>
2017-08-29 11:33:35 -0400

    Refactor enumerables

--- filter.rb
+++ filter.rb
@@ -1,33 +1,44 @@
+# def odd_integers(numbers)
+#   odds = []
+
+#   numbers.each do |number|
+#     odds << number if number.odd?
+#   end
+
+#   odds
+# end
 def odd_integers(numbers)
-  odds = []
-
-  numbers.each do |number|
-    odds << number if number.odd?
-  end
-
-  odds
+numbers.select {|number| number.odd?}
 end
+
+# def long_strings(strings, minimum_length)
+#   longs = []
+
+#   strings.each do |string|
+#     if string.length >= minimum_length
+#       longs << string
+#     end
+#   end
+
+#   longs
+# end
 
 def long_strings(strings, minimum_length)
-  longs = []
-
-  strings.each do |string|
-    if string.length >= minimum_length
-      longs << string
-    end
-  end
-
-  longs
+  strings.reject {|string|  string.length < minimum_length}
 end
 
+# def multiples_of(possible_multiples, number)
+#   multiples = []
+
+#   possible_multiples.each do |possible_multiple|
+#     if possible_multiple % number == 0
+#       multiples << possible_multiple
+#     end
+#   end
+
+#   multiples
+# end
+
 def multiples_of(possible_multiples, number)
-  multiples = []
-
-  possible_multiples.each do |possible_multiple|
-    if possible_multiple % number == 0
-      multiples << possible_multiple
-    end
-  end
-
-  multiples
+  possible_multiples.select{|possible_multiple| possible_multiple % number == 0}
 end
