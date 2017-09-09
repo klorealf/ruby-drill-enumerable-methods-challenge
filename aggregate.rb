1a52c8c6fa202aad2c8002eb5c46d2ce26e7ee19
Chris Camp <cpcamp2@gmail.com>
2017-09-09 15:27:57 -0400

    Release 3

--- aggregate.rb
+++ aggregate.rb
@@ -1,23 +1,7 @@
 def sum(numbers, starting_value = 0)
-  total = starting_value
-
-  numbers.each do |number|
-    total += number
-  end
-
-  total
+  numbers.reduce(starting_value) { |total, number| total += number }
 end
 
 def hyphenate(words)
-  hyphenated_words = ""
-
-  words.each do |word|
-    if hyphenated_words.empty?
-      hyphenated_words += word
-    else
-      hyphenated_words += "-#{word}"
-    end
-  end
-
-  hyphenated_words
+  words.reduce { |hyphenated_words,word| hyphenated_words += "-#{word}"}
 end
